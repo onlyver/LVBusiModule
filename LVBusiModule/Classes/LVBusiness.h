346756732535e64eb59bb403d8e21f5710e063ff
onlyver <1072539675@qq.com>
2023-03-10 12:15:08 +0800

    init

---
+++ LVBusiModule/Classes/LVBusiness.h
@@ -0,0 +1,16 @@
+//
+//  LVBusiness.h
+//  LVBusiModule
+//
+//  Created by LiLver on 2023/3/10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LVBusiness : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LVBusiModule/Classes/LVBusiness.m
@@ -0,0 +1,12 @@
+//
+//  LVBusiness.m
+//  LVBusiModule
+//
+//  Created by LiLver on 2023/3/10.
+//
+
+#import "LVBusiness.h"
+
+@implementation LVBusiness
+
+@end
